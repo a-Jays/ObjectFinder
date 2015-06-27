0384bf9d66290c877c21f7e239be9cb2a95d0323
Ajay Shankar <ajays@iiitd.ac.in>
2015-06-27 15:11:08 +0530

    Update findObjects.m
    added input argument test.

--- findObjects.m
+++ findObjects.m
@@ -12,6 +12,10 @@ function mine_regions = findObjects( img )
 	
 	% `mine_regions` is a cell-array of final ROIs.
 	
+	if nargin ~= 1
+		fprintf('Provide one (1) (ek) image as argument.');
+	end
+	
 	img = medfilt2( rgb2gray(img), [3 3] );
 	
 	img = img.^1.1;
@@ -22,6 +26,6 @@ function mine_regions = findObjects( img )
 	retained_centres = getCentres( retained_C );
 	boundingboxes = getBoundingBox( img, retained_centres, 1 );
 	
-	mine_regions = SURF_test( boundingboxes );
+	mine_regions = SURFTest( boundingboxes );
 	
 end	
