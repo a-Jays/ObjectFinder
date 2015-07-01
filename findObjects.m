9d831b204e32fa70471936877f76b34262b10d79
Ajay Shankar <ajays@iiitd.ac.in>
2015-07-01 14:13:36 +0530

    Update findObjects.m
    Edited description; im2double;

--- findObjects.m
+++ findObjects.m
@@ -1,7 +1,10 @@
 function mine_regions = findObjects( img )
 
-	% top-level function that finds mine and mine-like objects in a SONAR image.
-	% takes `img` the image as an argument, cleans it (and possibly applies a gamma correction to it).
+	% top-level function to find mine & mine-like objects in SONAR image.
+	% - takes `img` the colour-image as an argument, cleans it and applies
+	% gamma correction to it. Converts to `double` for `contourc`.
+	% - also takes a database, `DB`, which is a cell-array of features from
+	% different images, to look into.
 	
 	% then follow this pipeline:
 	% 	- find all level-1 contours in the image.
@@ -12,21 +15,23 @@ function mine_regions = findObjects( img )
 	
 	% `mine_regions` is a cell-array of final ROIs.
 	
-	if nargin ~= 1
-		fprintf('Provide one (1) (ek) image as argument.');
+	if nargin < 2
+		fprintf('Provide one (1) (ek) image and the database as argument. See help.\n');
 	end
 	
+	% The following is with the hope that one fine day, when the sun is bright,
+	% the code can be separated for Octave, using the ENVT (environment) parameter.
 	if exist( 'OCTAVE_VERSION', 'builtin' )
 		ENVT = 'Octave';
 	else
 		ENVT = 'Matlab';
 	end
 	
-	img = medfilt2( rgb2gray(img), [3 3] );
+	img = im2double( medfilt2( rgb2gray(img), [3 3] ) );
 	
 	img = img.^1.1;
 	
-	C = contourc( img, [1 1] );
+	C = contourc( img, 1 );
 	
 	retained_C = selectContours( C );
 	retained_centres = getCentres( retained_C );
