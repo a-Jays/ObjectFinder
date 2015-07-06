269ea4f7b7841d3a48ff2548a372d88dae8b3284
Ajay Shankar <ajays@iiitd.ac.in>
2015-07-06 20:54:44 +0530

    Update findObjects.m

--- findObjects.m
+++ findObjects.m
@@ -1,4 +1,4 @@
-function mine_regions = findObjects( img, DB )
+function mine_regions = findObjects( img, DB, Method )
 
 	% top-level function to find mine & mine-like objects in SONAR image.
 	% - takes `img` the colour-image as an argument, cleans it and applies
@@ -12,11 +12,15 @@ function mine_regions = findObjects( img, DB )
 	% 	- obtain a region of interest around each contour.
 	% 	- find SURF keypoint descriptors in each ROI.
 	% 	- retain those ROIs which have some `N` matches.
-	
+	%
+	% The argument `method` specifies which method to use: =0 for contour-based
+	% and =1 for intensity-based thresholding. Defaulted to 1.
 	% `mine_regions` is a cell-array of final ROIs.
-	
-	if nargin < 2
-		fprintf('Provide one (1) (ek) image and the database as argument. See help.\n');
+	if nargin < 3
+		Method = 1;
+		if nargin < 2
+			fprintf('Provide one (1) (ek) image and the database as argument. See help.\n');
+		end
 	end
 	
 	% The following is with the hope that one fine day, when the sun is bright,
@@ -28,15 +32,20 @@ function mine_regions = findObjects( img, DB )
 	end
 	
 	img = im2double( medfilt2( rgb2gray(img), [3 3] ) );
-	
 	img = img.^1.1;
 	
-	C = contourc( img, 1 );
-	
-	retained_C = selectContours( C );
-	retained_centres = getCentres( retained_C );
+	if Method == 1
+		Components =  bwconncomp( wiener2( bwareaopen( im2bw( img, 0.4 ), 100 ), [5 5] ) );
+        	retained_centres = struct2cell( regionprops( Components, 'Centroid' ) );
+        else
+		C = contourc( img, 1 );
+		retained_C = selectContours( C );
+		retained_centres = getCentres( retained_C );
+	end
 	boundingboxes = getBoundingBox( img, retained_centres, 1 );
-	
-	mine_regions = SURFTest( boundingboxes, DB );
+	fprintf( 'Candidate ROIs: %d\n', size( boundingboxes, 2 ) );
+	mine_indices = SURFTest( boundingboxes, DB );
+	mine_regions = boundingboxes( mine_indices == 1 );
+    	fprintf( 'SURF selected ROIs: %d\n', size( mine_regions, 2 ) );
 	
 end	
